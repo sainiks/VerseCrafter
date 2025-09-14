eff8bdc4735321dbb1b984638b8e1c939fc47be5
Kunal Saini <66879424+sainiks@users.noreply.github.com>
2025-09-14 22:15:29 +0530

    Modify favoriteActivity and favoritePlace options

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ import { generateTextAndImage } from "./utils.js"
 let name = "Kunal Saini"
 
 // 2. Change the value of the variable to your favorite activity
-let favoriteActivity = "Coding and listening lofi music"
+let favoriteActivity = "Coding" or "listening" or "lofi music"
 
 // 3. Assign the favoritePlace variable your favorite place
 // I.e. city, mountain, pub, forrest, beach, Manhattan, etc.
-let favoritePlace = "coffee cafe in hills"
+let favoritePlace = "coffee" or "cafe" or "hills"
 
 // 4. Configure the AI by setting a temperature from 0 to 1
 // The higher temperature, the more random & experimental output
